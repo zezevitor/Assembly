c5b958c94b33173201ca64b0f8ddb0575f36fe01
Andre Vitor <andrevbm.288@gmail.com>
2024-09-30 13:23:28 -0300

    Inicio print de número inteiro

--- assembly.s
+++ assembly.s
@@ -3,13 +3,25 @@
 .global _start
 
 _start:
-    movw $10, %cx
+	movw $54321, %ax	# AX = 54321
+	movw $10000, %si	# SI = 10000
 
 loop:
-	call print
-	decw %cx
-	jz loop_fim
-	jmp loop
+	cmpw $0, %si		# Compara SI com 0
+	je loop_fim			# Pula pro loop_fim se igual
+
+	call print			# Chama a função print
+
+	movw %di, %ax		# Salva novo AX em DI
+
+	movw %si, %ax		# Salva SI em AX (dividendo)
+	movw $10, %cx		# CX = 10 (divisor)
+	div %cx				# Divide AX por CX; quociente em AX, resto em DX
+	movw %ax, %si		# Atualiza SI com o quociente
+
+	movw %di, %ax		# Restaura AX de DI após a divisão
+
+	jmp loop			# Recomeça o loop
 
 print:
 	movb $'x', %al
